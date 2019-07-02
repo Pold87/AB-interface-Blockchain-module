a911689a5f1769b4ea4da2977bbeb6415f349cfd
Volker Strobel <volker.strobel87@gmail.com>
2019-07-02 19:08:17 +0200

    Updated version, just good detection is missing

--- geth/scripts/start.sh
+++ geth/scripts/start.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -e
-#cd /root/eth-net-intelligence-api
-#perl -pi -e "s/XXX/$(hostname)/g" app.json
-#pm2 start ./app.json
-#sleep 3
 geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
-sleep 3
 BOOTSTRAP_IP=`cat /root/shared/my_enode.enode | tr -d '"'`
 GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
 echo "$GETH_OPTS"
 geth $GETH_OPTS&
 sleep 7
+bash /root/exec_template.sh "/root/templates/createAccount.txt"
+bash /root/exec_template.sh "/root/templates/setEtherbase.txt"
 bash /root/get_enode.sh
 bash /root/exec_template.sh "/root/templates/unlockAccount.txt"
 tail -f /dev/null
